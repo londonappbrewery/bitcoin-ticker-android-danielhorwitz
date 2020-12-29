d36a9deec4f0a23f804ea435247fb4a42b2efd5c
Daniel Horwitz <dhorwitz@liveperson.com>
2020-12-29 08:38:21 -0600

    Did assignment simply, but captured last price instead of asking price. You now have to sign up and get a key. The free account lasts for a few days.

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -12,16 +12,24 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
 
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Locale;
+
+import cz.msebera.android.httpclient.Header;
 
 
 public class MainActivity extends AppCompatActivity {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC";
+    private final String AUTH_KEY = "NWY0MTgxOGYwYjk5NDE1MWExOWY3YWM2Yzg2NTk2YjY";
 
     // Member Variables:
     TextView mPriceTextView;
@@ -31,8 +39,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mPriceTextView = (TextView) findViewById(R.id.priceLabel);
-        Spinner spinner = (Spinner) findViewById(R.id.currency_spinner);
+        mPriceTextView = findViewById(R.id.priceLabel);
+        Spinner spinner = findViewById(R.id.currency_spinner);
 
         // Create an ArrayAdapter using the String array and a spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -44,33 +52,66 @@ public class MainActivity extends AppCompatActivity {
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
 
-        // TODO: Set an OnItemSelected listener on the spinner
+        // The OnItemSelected listener for the spinner.
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("Bitcoin", "+spinner.onItemSelected(,," + position + "," + id + ")");
+                String selectedCur = (String) parent.getItemAtPosition(position);
+                Log.d("Bitcoin", "" + selectedCur);
+                String url = BASE_URL + selectedCur;
+                letsDoSomeNetworking(url);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.d("Bitcoin", "Nothing selected.");
+            }
+        });
 
     }
 
-    // TODO: complete the letsDoSomeNetworking() method
+    // Query for BitCoin price.
     private void letsDoSomeNetworking(String url) {
+        Log.d("Bitcoin", "+letsDoSomeNetworking(" + url + ")");
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.addHeader("x-ba-key", AUTH_KEY);
+        RequestParams params = new RequestParams();
+        client.get(url, params, new JsonHttpResponseHandler() {
 
-//        AsyncHttpClient client = new AsyncHttpClient();
-//        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
-//
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                // called when response HTTP status is "200 OK"
-//                Log.d("Clima", "JSON: " + response.toString());
-//                WeatherDataModel weatherData = WeatherDataModel.fromJson(response);
-//                updateUI(weatherData);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-//                Log.d("Clima", "Request fail! Status code: " + statusCode);
-//                Log.d("Clima", "Fail response: " + response);
-//                Log.e("ERROR", e.toString());
-//                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // called when response HTTP status is "200 OK"
+                Log.d("Bitcoin", "JSON: " + response.toString());
+                try {
+                    double lastPrice = response.getDouble("last");
+                    Log.d("Bitcoin", "Last price: " + lastPrice);
+                    String lastPriceString = String.format(Locale.getDefault(), "%,.2f", lastPrice);
+                    Log.d("Bitcoin", "Last price string: " + lastPriceString);
+                    mPriceTextView.setText(lastPriceString);
+                } catch (JSONException jex) {
+                    Log.d("Bitcoin", "Error parsing response: " + jex.getMessage());
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                Log.d("Bitcoin", "Request fail! Status code: " + statusCode);
+                Log.d("Bitcoin", "Fail response: " + response);
+                Log.e("ERROR", e.toString());
+                Toast.makeText(MainActivity.this, "Request Failed", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                //super.onFailure(statusCode, headers, responseString, throwable);
+                Log.d("Bitcoin", "String/Throwable Request fail! Status code: " + statusCode);
+                Log.d("Bitcoin", "Fail response: " + responseString);
+                Log.e("ERROR", throwable.toString());
+                Toast.makeText(MainActivity.this, "Request Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
